a3955f2c04a81d7d6cd44a300406aafa5f51530c
sachins <39sachen@gmail.com>
2018-04-24 13:47:48 +0300

    first comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from './componnents/Layout'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+       <layout title="Chat Application"/>
+      </div>
+    );
+  }
+}
+
+export default App;
